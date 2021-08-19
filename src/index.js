02fa017e90692a6204e3f11a9561bcb97507b770
Gaurav <gauravgusain48@gmail.com>
2021-08-20 01:09:42 +0530

    commit2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import firebase from "firebase/app";
+import firebase from "firebase";
 import  "firebase/firestore";
 import  "firebase/auth";
 
